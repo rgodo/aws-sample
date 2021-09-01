a7ae7ee64118e0aab1b84e3e2e8a7dd7c9dc4c96
Rodrigo Godo <rodrigogodo@gmail.com>
2021-09-01 17:10:24 -0500

    Cambio de puerto al 80

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello world with express in EC2!</h1>');
 });
 
-app.listen(8080);
+app.listen(80);
 
-console.log("server on port 8080");
+console.log("server on port 80");
